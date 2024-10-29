7952ef2dfbe8b68f2de21bfcf9a928d963006b71
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-29 21:54:19 +1100

    Fixed mutations.js to match backend graphql schema

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,8 +2,8 @@ import { gql } from '@apollo/client';
 
 // GraphQL mutation to login a user
 export const LOGIN_USER = gql`
-    mutation loginUser($email: String!, $password: String!) {
-        loginUser(email: $email, password: $password) {
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
             token
             user {
                 _id
@@ -49,6 +49,20 @@ export const DELETE_BOOK = gql`
                 title
                 image
                 link
+            }
+        }
+    }
+`;
+
+// GraphQL mutation to create a new user
+export const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
             }
         }
     }
