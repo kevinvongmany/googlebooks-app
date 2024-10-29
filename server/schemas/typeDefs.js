10570b760bb51ca1d29a3d90f722c9fda851de22
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-29 21:50:07 +1100

    Added typeDefs for schema

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  
+  type Query {
+    me: User
+  }
+
+  input BookInput {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    createUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    deleteBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
