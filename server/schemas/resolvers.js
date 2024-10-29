781e3ff1bda965bb2ae795d4add0c0ff7c6e6792
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-29 21:50:34 +1100

    Added resolvers for schema

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,63 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('savedBooks');
+
+        return userData;
+      }
+
+      throw new Error('Not logged in');
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {    
+        const user = await User.findOne({ $or: [{ email: email }] });
+        if (!user) {
+        return res.status(400).json({ message: "Can't find this user" });
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+        return res.status(400).json({ message: 'Wrong password!' });
+        }
+        const token = signToken(user);
+        return { token, user };
+        
+    },
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $addToSet: { savedBooks: bookData } },
+            { new: true }
+            );
+            return updatedUser;
+        }
+    },
+    deleteBook: async (parent, { bookId }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: { savedBooks: { bookId: bookId } } },
+            { new: true }
+            );
+            return updatedUser;
+        }
+    },
+  }
+}
+
+module.exports = resolvers;
